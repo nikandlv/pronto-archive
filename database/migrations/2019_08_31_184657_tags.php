1aad91fc8b77f619965ec683f85584e1523d7033
Nikan Dalvand <nikandalvand@gmail.com>
2019-08-31 23:31:48 +0430

    Add tags table

--- database/migrations/2019_08_31_184657_tags.php
+++ database/migrations/2019_08_31_184657_tags.php
@@ -13,7 +13,10 @@ class Tags extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('tags', function(Blueprint $table) {
+            $table->bigIncreaments('id');
+            $table->string('title',255);
+        });
     }
 
     /**
@@ -23,6 +26,6 @@ class Tags extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('tags');
     }
 }

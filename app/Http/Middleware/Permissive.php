d4305e1fa30b96c822a19de2f34051b817d60f05
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-06 19:24:13 +0000

    Added base logic of permissive

--- app/Http/Middleware/Permissive.php
+++ app/Http/Middleware/Permissive.php
@@ -4,7 +4,11 @@ namespace App\Http\Middleware;
 use Closure;
 
 class Permissive {
-   public function handle($request, Closure $next) {
+   public function handle($request, Closure $next, $roles) {
+     $user = $request->user();
+     if(in_array($user->role,$roles)) {
+       
+     }
       return $next($request);
    }
 }

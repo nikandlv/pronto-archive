bcd5852538a49ad2c6c30ef1c0b229d0b0648d99
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-06 19:24:39 +0000

    Added abort

--- app/Http/Middleware/Permissive.php
+++ app/Http/Middleware/Permissive.php
@@ -7,7 +7,7 @@ class Permissive {
    public function handle($request, Closure $next, $roles) {
      $user = $request->user();
      if(in_array($user->role,$roles)) {
-       
+       abort(403);
      }
       return $next($request);
    }

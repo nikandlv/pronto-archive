371991a9f7f45ddec1825535483d24687c1c9149
noisyBoy-9 <sina.shariati@yahoo.com>
2019-09-13 16:22:58 +0430

    added self model relationship for categories

--- app/Category.php
+++ app/Category.php
@@ -12,4 +12,9 @@ class Category extends Model
     {
         return $this->belongsToMany(Post::class ,'post_category' , 'category_id' , 'post_id');
     }
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
 }

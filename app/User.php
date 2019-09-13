8e5e4d39b8034c39e0fc4961480f1c3d8abdcdd9
noisyBoy-9 <sina.shariati@yahoo.com>
2019-09-13 16:09:10 +0430

    added model relationship for UserMeta and User

--- app/User.php
+++ app/User.php
@@ -37,4 +37,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function metas()
+    {
+        return $this->hasMany(UserMeta::class);
+    }
 }

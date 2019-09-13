85f33d2c533d2a1e15bd4a445d7dd42139982b15
noisyBoy-9 <sina.shariati@yahoo.com>
2019-09-13 16:35:15 +0430

    added model relationship for Comment and Post

--- app/Post.php
+++ app/Post.php
@@ -21,4 +21,9 @@ class Post extends Model
     {
         return $this->belongsToMany(Category::class , 'post_category', 'post_id' , 'category_id');
     }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }

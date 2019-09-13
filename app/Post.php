747988bbffbcfa9f4433d5d1e75a8700335cfa1f
noisyBoy-9 <sina.shariati@yahoo.com>
2019-09-13 16:17:31 +0430

    added model relationship for Tag and Post

--- app/Post.php
+++ app/Post.php
@@ -11,4 +11,9 @@ class Post extends Model
     public function attachments() {
         return $this->hasMany(Attachment::class);
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class , 'post_tag' , 'user_id' , 'post_id');
+    }
 }
--- app/Tag.php
+++ app/Tag.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     protected $fillable = ['title'];
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class, 'post_tag' ,'tag_id' , 'post_id');
+    }
 }

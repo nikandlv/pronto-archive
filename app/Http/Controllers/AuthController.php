d2941bfa91720aaaebde1223310680ae9e676f81
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-15 22:32:48 +0300

    Added controller extend to auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,6 +1,13 @@
 <?php
+
 namespace App\Http\Controllers;
 
-class AuthController {
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+
+class AuthController extends Controller {
+
     
+
 }

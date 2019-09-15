382c6af89677d52f502672c682977ef6fd79433b
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-15 23:58:13 +0430

    Added response builder

--- app/Helpers/ResponseBuilder.php
+++ app/Helpers/ResponseBuilder.php
@@ -3,5 +3,19 @@ namespace App\Helpers;
 
 class ResponseBuilder {
 
+    static public function build($code,$params,$data,$status,$lang='fa') {
+        if(is_null($params)) {
+            $params = [];
+        }
+        $responseStatus = false;
+        if($status >= 200 && $status < 400){
+            $responseStatus = true;
+        }
+       return response()->json([ 
+           "message" => trans($code, $params, $lang),
+           "data" => $data,
+           "status" => $responseStatus
+       ],$status);
+    }
  
 }

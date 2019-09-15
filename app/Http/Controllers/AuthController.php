c91a6e10c6c953efc929cb030f2b29d4175d3f48
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-15 22:40:03 +0300

    Implemented Signin function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,21 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\ResponseBuilder;
 use App\Traits\PassportToken;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller {
 
     use PassportToken;
 
     function Signup(Request $request) {
-        
+
     }
     
     function Signin(Request $request) {
-        
+        $credentials = $request->validate([
+            'email' => 'sometimes|string',
+            'username' => 'sometimes|string',
+            'password' => 'required|string'
+        ]);
+        if(!Auth::attempt($credentials))
+            return ResponseBuilder::build('Wrong credentials',null,null, 401);
+        $user = $request->user();
+        $authorization = $this->authorizeClient($user, false);
+        $authorization['expires_in'] = Carbon::now()->addSeconds($authorization['expires_in']);
+        return ResponseBuilder::build('response.SUCCESS',null,
+            $authorization
+        ,200);
     }
 
     function Signout(Request $request) {
@@ -24,7 +38,7 @@ class AuthController extends Controller {
     }
 
     function refresh(Request $request) {
-        
+
     }
 
 }

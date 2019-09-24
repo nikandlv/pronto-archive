7268c940dead286e813ae3d9f87e0e69fb8db9b0
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-24 23:39:53 +0000

    Added Home import

--- resources/jsx/Routes/MainRouter.jsx
+++ resources/jsx/Routes/MainRouter.jsx
@@ -1,5 +1,6 @@
 import router from 'react-router-dom'
 import {Switch} from 'react-router-dom'
+import Home from '../Layouts/Blog/Home'
 export default function MainRouter() {
     return (
         <Switch>

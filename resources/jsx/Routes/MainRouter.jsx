db4a1b00ecb1cc81d92484a079863a19e20db346
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-25 22:43:22 +0000

    Added about route

--- resources/jsx/Routes/MainRouter.jsx
+++ resources/jsx/Routes/MainRouter.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import {Switch, Route, BrowserRouter} from 'react-router-dom'
 import Home from '../Layouts/Blog/Home'
+import About from '../Layouts/Blog/About'
 export default function MainRouter() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" component={Home} exact />
+                <Route path="/about" component={About} exact />
             </Switch>
         </BrowserRouter>
     )

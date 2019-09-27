08f429f2489c768d0665ff3765b6165a6c97c53b
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-26 23:38:01 -0500

    Added router for blog,admin,panel

--- resources/jsx/Routes/MainRouter.jsx
+++ resources/jsx/Routes/MainRouter.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import {Switch, Route, BrowserRouter} from 'react-router-dom'
 import Home from '../Layouts/Blog/Home'
 import About from '../Layouts/Blog/About'
+import Wrapper from '../Layouts/Wrapper'
 export default function MainRouter() {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" component={Home} exact />
-                <Route path="/about" component={About} exact />
-            </Switch>
-        </BrowserRouter>
+        <Wrapper>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/" component={Home} exact />
+                    <Route path="/about" component={About} exact />
+                </Switch>
+            </BrowserRouter>
+        </Wrapper>
     )
 }

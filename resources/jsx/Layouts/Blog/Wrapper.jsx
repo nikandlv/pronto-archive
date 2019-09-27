7ccc8f99d3f646ef689a3c0a768ffde60b5e9160
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-27 17:08:18 -0500

    Added grid layout to blog

--- resources/jsx/Layouts/Blog/Wrapper.jsx
+++ resources/jsx/Layouts/Blog/Wrapper.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import Topbar from './Topbar'
+import Grid from '@material-ui/core/Grid'
 export default function Wrapper(props) {
     return (
         <section>
             <Topbar />
+            <Grid container spacing={2}>
+                <Grid xs={12} lg={4} xlg={3} item>
+                    
+                </Grid>
+                <Grid xs={12} lg={8} xlg={9} item>
+                    
+                </Grid>
+            </Grid>
         </section>
     )
 }

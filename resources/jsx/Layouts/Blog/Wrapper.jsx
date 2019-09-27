a7d6ae28dc68521ddd2bdf07658d4c36588c6385
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-27 17:10:21 -0500

    Added sidebar and fixed sizing on grid

--- resources/jsx/Layouts/Blog/Wrapper.jsx
+++ resources/jsx/Layouts/Blog/Wrapper.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Topbar from './Topbar'
 import Grid from '@material-ui/core/Grid'
+import Sidebar from './Sidebar'
 export default function Wrapper(props) {
     return (
         <section>
             <Topbar />
             <Grid container spacing={2}>
-                <Grid xs={12} lg={4} xlg={3} item>
-                    
+                <Grid xs={12} lg={3} xlg={4} item>
+                    <Sidebar />
                 </Grid>
-                <Grid xs={12} lg={8} xlg={9} item>
-                    
+                <Grid xs={12} lg={9} xlg={8} item>
+                    a
                 </Grid>
             </Grid>
         </section>

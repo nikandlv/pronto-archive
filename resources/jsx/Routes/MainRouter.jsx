b309a11cdedfba26267d263442c655347f14a8fc
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-26 23:41:46 -0500

    Wire the routers

--- resources/jsx/Routes/MainRouter.jsx
+++ resources/jsx/Routes/MainRouter.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import {Switch, Route, BrowserRouter} from 'react-router-dom'
-import Home from '../Layouts/Blog/Home'
-import About from '../Layouts/Blog/About'
+
 import Wrapper from '../Layouts/Wrapper'
+import BlogRouter from './BlogRouter'
+import AdminRouter from './AdminRouter'
+import PanelRouter from './PanelRouter'
+
 export default function MainRouter() {
     return (
         <Wrapper>
             <BrowserRouter>
                 <Switch>
-                    <Route path="/" component={Home} exact />
-                    <Route path="/about" component={About} exact />
+                    <Route path="/panel/*" component={AdminRouter}/>
+                    <Route path="/admin/*" component={PanelRouter}/>
+                    <Route path="/*" component={BlogRouter}/>
                 </Switch>
             </BrowserRouter>
         </Wrapper>

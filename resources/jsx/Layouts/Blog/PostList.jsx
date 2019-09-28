53f394c39a984d35ac87e51ecea8f04848b25465
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-28 17:03:53 -0500

    Added home page sample

--- resources/jsx/Layouts/Blog/PostList.jsx
+++ resources/jsx/Layouts/Blog/PostList.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Paper } from '@material-ui/core'
 
 export default function PostList() {
     return (
-        <div>
-            
-        </div>
+        <section>
+            <Paper>a</Paper>
+        </section>
     )
 }

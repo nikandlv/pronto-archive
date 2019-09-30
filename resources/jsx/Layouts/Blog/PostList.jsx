e5a03d0d1541f898c14729d3135181c03c6d0ef2
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-30 16:49:24 -0500

    Added viewday icon to postlist

--- resources/jsx/Layouts/Blog/PostList.jsx
+++ resources/jsx/Layouts/Blog/PostList.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import { Paper } from '@material-ui/core'
+import { IconButton } from '@material-ui/core'
+
+import ViewDay from '@material-ui/icons/ViewDayOutlined'
 
 export default function PostList() {
     return (
         <section>
-            <Paper>a</Paper>
+            <div>
+                <IconButton>
+                    <ViewDay/>
+                </IconButton>
+            </div>
         </section>
     )
 }

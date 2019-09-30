cf2239e41b3909a0fefd28832cb67f5a349a75d9
Nikan Dalvand <nikandalvand@gmail.com>
2019-09-30 16:50:13 -0500

    Added style to postlist

--- resources/jsx/Layouts/Blog/PostList.jsx
+++ resources/jsx/Layouts/Blog/PostList.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import { IconButton } from '@material-ui/core'
+import { IconButton, makeStyles } from '@material-ui/core'
 
 import ViewDay from '@material-ui/icons/ViewDayOutlined'
 
+const useStyles = makeStyles({
+    header: {
+
+    }
+})
+
 export default function PostList() {
+    const styles = useStyles()
     return (
         <section>
             <div>

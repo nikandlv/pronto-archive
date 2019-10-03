20fe9b78a3846e7b616d5674f938999c76873d75
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-03 05:46:52 -0600

    Language icon now opens the menu

--- resources/jsx/Layouts/Blog/Topbar.jsx
+++ resources/jsx/Layouts/Blog/Topbar.jsx
@@ -46,13 +46,15 @@ function Topbar(props) {
           <Typography variant="h6" className={classes.title}>
             {name}
           </Typography>
-          <IconButton color="inherit" onClick={() => {
-
+          <IconButton color="inherit" onClick={(event) => {
+            setLanguageEl(event.currentTarget)
           }}>
               <LanguageIcon />
           </IconButton>
           
-          <Menu open={Boolean(languageEl)} anchorEl={languageEl}>
+          <Menu open={Boolean(languageEl)} anchorEl={languageEl} onClose={() => {
+            setLanguageEl(null)
+          }}>
               <MenuItem>EN</MenuItem>
               <MenuItem>ES</MenuItem>
           </Menu>

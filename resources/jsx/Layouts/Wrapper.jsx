e894d9cbee9daac8774f2ce7e061f4c00612552a
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-06 19:36:31 -0400

    Added light theme

--- resources/jsx/Layouts/Wrapper.jsx
+++ resources/jsx/Layouts/Wrapper.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
-const theme = createMuiTheme({
-    palette: {
-        type: 'light'
-    },  
-});
+const theme = {
+    light: createMuiTheme({
+        palette: {
+            type: 'light'
+        },  
+    }),
+    dark: createMuiTheme({
+        palette: {
+            type: 'dark'
+        },  
+    })
+};
 export default function Wrapper(props) {
     return (
-        <ThemeProvider theme={theme}>
+        <ThemeProvider theme={theme.light}>
             <CssBaseline />
             {props.children}
         </ThemeProvider>

0d264121976c6dd8991171d3189f5505eede0ea6
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-12 19:39:22 -0500

    Added styles to github widget

--- resources/jsx/Components/GithubWidget.jsx
+++ resources/jsx/Components/GithubWidget.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { Paper } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
+
+const useStyles = makeStyles({
+    paper: {
+        borderRadius: 16
+    }
+})
 
 export default function GithubWidget() {
+    const styles = useStyles()
     return (
-        <Paper>
+        <Paper className={styles.paper}>
             a
         </Paper>
     )

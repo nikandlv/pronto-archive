76f8c52a151a0d80a39839cb0b68a88e4dbb3d17
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-12 20:03:23 -0500

    Added github contrib calendar

--- resources/jsx/Components/GithubWidget.jsx
+++ resources/jsx/Components/GithubWidget.jsx
@@ -4,6 +4,19 @@ import { makeStyles } from '@material-ui/styles'
 import ArrowDown from '@material-ui/icons/ExpandMoreOutlined'
 import ArrowUp from '@material-ui/icons/ExpandLessOutlined'
 import GithubIcon from './Icons/GithubIcon'
+import 'react-github-contribution-calendar/default.css'
+import Calendar from 'react-github-contribution-calendar';
+var panelColors = ['#EEEEEE', '#D6E685', '#8CC665', '#44A340', '#1E6823'];
+
+var values = {};
+var pad = (v) => (v < 10 ? '0' + v : v)
+for (var i = 1; i <= 12; i++) {
+    for (var j = 1; j <= 31; j++) {
+    values['2016-' + pad(i) + '-' + pad(j)] = Math.floor(Math.random() * 5);
+    }
+}
+    var until = '2016-12-31';
+
 const useStyles = makeStyles({
     paper: {
         borderRadius: 16,
@@ -30,6 +43,8 @@ export default function GithubWidget() {
             </IconButton>
                     <ListItemText primary="Github activity" />
                 </ListItem>
+
+                <Calendar panelColors={panelColors} values={values} until={until} />
             <List className={styles.list}>
             <Collapse in={expand} collapsedHeight='160px'>
                 <ListItem button>

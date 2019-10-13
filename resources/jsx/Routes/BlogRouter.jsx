236cb701e7d3fc1b802af7150ee130b536327ad3
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-14 03:13:23 +0330

    Added react-pose and posed route container

--- resources/jsx/Routes/BlogRouter.jsx
+++ resources/jsx/Routes/BlogRouter.jsx
@@ -4,6 +4,13 @@ import Home from '../Layouts/Blog/Home'
 import About from '../Layouts/Blog/About'
 import Wrapper from '../Layouts/Blog/Wrapper'
 import Post from '../Layouts/Blog/Post'
+import posed, { PoseGroup } from 'react-pose';
+
+
+const RouteContainer = posed.div({
+    enter: { x: 0, opacity: 1,    beforeChildren: true,   },
+    exit: { x: -100, opacity: 0 }
+});
 
 export default function BlogRouter() {
     return (

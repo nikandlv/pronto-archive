e3588450b8c0ef53eb3c868450435fdeb78747a5
Nikan Dalvand <nikandalvand@gmail.com>
2019-10-13 12:18:18 -0500

    Added paper style to post

--- resources/jsx/Layouts/Blog/Post.jsx
+++ resources/jsx/Layouts/Blog/Post.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import Comments from './Comments'
+import { Card, CardContent, makeStyles } from '@material-ui/core'
+
+const useStyles = makeStyles({
+    paper: {
+        borderRadius: 16
+    }
+})
+
 export default function Post() {
+    const styles = useStyles()
     return (
         <div>
-            
+            <Card className={styles.paper}>
+                <CardContent>
+                    abcsssssssssssssssss
+                </CardContent>
+            </Card>
             <Comments />
         </div>
     )
